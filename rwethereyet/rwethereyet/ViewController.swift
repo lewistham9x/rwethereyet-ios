628022210944406af64ea412831895e9e7514858
Lewis Tham <lewistham@Lewiss-MacBook-Pro.local>
2018-01-08 17:37:17 +0800

    Initial Commit

---
+++ rwethereyet/rwethereyet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rwethereyet
+//
+//  Created by Lewis Tham on 8/1/18.
+//  Copyright © 2018 somethingwithc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
