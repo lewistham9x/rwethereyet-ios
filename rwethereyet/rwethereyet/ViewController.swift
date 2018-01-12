e54b310ab0f5f1282a0640718acd2fbf1704d50e
Lewis Tham <lewistham9x@gmail.com>
2018-01-12 21:24:52 +0800

    CoreData for busrouter.sg API

--- rwethereyet/rwethereyet/ViewController.swift
+++ rwethereyet/rwethereyet/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let appDelegate = (UIApplication.shared.delegate) as! AppDelegate
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    
+    func initBusData(){ //must change to run somewhereinside appdelegate thing to update or save all bus api data into coredata
+        let context = self.appDelegate.persistentContainer.viewContext
+        //grab all bus service numbers
+        //query each bus service
+        //for each route
+        //for each bus stop number in the service
+        //query again for the bus stop object with said number
+        //create a bus stop object if it does not exist, and then add the bus stop object to the bus service route's list
+        //save into core data
+        //using relationships, should be able to view the route of bus stops for a particular service
+        //should be able to view the bus services of a particular bus stop
+        
+        
+        
+        //need to clear all data temporarily so no duplicates will be saved
+        
+    }
 }
 
