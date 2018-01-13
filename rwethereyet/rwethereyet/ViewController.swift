5f15d6f0d1dd2e7b9a4a9388cc5499f578a9a1fd
Lewis Tham <lewistham9x@gmail.com>
2018-01-13 14:30:59 +0800

    Integration of API for bus stop list (works so far)

--- rwethereyet/rwethereyet/ViewController.swift
+++ rwethereyet/rwethereyet/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Alamofire
+import AlamofireObjectMapper
 
 class ViewController: UIViewController {
 
@@ -15,6 +17,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        initBusData()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,12 +30,36 @@ class ViewController: UIViewController {
     
     
     func initBusData(){ //must change to run somewhereinside appdelegate thing to update or save all bus api data into coredata
-        let context = self.appDelegate.persistentContainer.viewContext
+        //let context = self.appDelegate.persistentContainer.viewContext
         //initialise and create all bus stops
+        
+        let busStopsURL = "https://raw.githubusercontent.com/cheeaun/busrouter-sg/master/data/2/bus-stops.json" //"https://busrouter.sg/data/2/bus-stops.json"
+        print(busStopsURL)
+        Alamofire.request(busStopsURL).responseArray { (response: DataResponse<[RouteBusStop]>) in
+            
+            let routeBusStopArray = response.result.value
+            
+            if let routeBusStopArray =  routeBusStopArray {
+                
+                //let busStopArray =
+                //let busStop = BusStop(context : context)
+                
+                for routeStop in routeBusStopArray {
+                    //busStop.latitude = routeStop.latitude
+                    print("test")
+                }
+            }
+        }
+        
+        
+        
+        
+        
+        /*
         let busStop = BusStop(context : context)
-        busStop.stopNo = 0
+        busStop.stopNo = "0"
         
-        
+        */
         //https://github.com/tristanhimmelman/AlamofireObjectMapper
         
         /*
---
+++ rwethereyet/rwethereyet/model/API/RouteBusStop.swift
@@ -0,0 +1,30 @@
+//
+//  RouteBusStop.swift
+//  rwethereyet
+//
+//  Created by Lewis Tham on 13/1/18.
+//  Copyright © 2018 somethingwithc. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+//map API json  to RouteBusStop objects, to add into CoreData as BusStop objects later
+
+class RouteBusStop: Mappable {
+    var stopNo:String?
+    var latitude: String?
+    var longitude: String?
+    var name: String?
+    
+    required init?(map: Map){
+        
+    }
+    
+    func mapping(map: Map) {
+        stopNo <- map["no"]
+        latitude <- map["lat"]
+        longitude <- map["lng"]
+        name <- map["name"]
+    }
+}
